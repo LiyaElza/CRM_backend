3f06831274fdd4b0868e53631f1e6132c48b41ef
Liya Elsa Mathew <liya.mathew@experionglobal.com>
2022-11-24 09:01:21 +0530

    second commit on reports

--- reports/reportsapp/models.py
+++ reports/reportsapp/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class monthlyanalysis(models.Model):
     monthlist = models.CharField(max_length = 20)
     sales = models.IntegerField()
+
+class productanalysis(models.Model):
+    producttype=models.CharField(max_length=20)
+    sales=models.IntegerField()
