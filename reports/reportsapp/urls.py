3be4be16561e65fdd44052e66bce9335699f45da
Liya Elsa Mathew <liya.mathew@experionglobal.com>
2022-11-25 00:52:12 +0530

    Second Commit by Liya

--- reports/reportsapp/urls.py
+++ reports/reportsapp/urls.py
@@ -5,4 +5,6 @@ urlpatterns=[
     path('',views.getRoutes,name="routes"),
     path('monthlysales/',views.getmonthlySales,name="monthlysales"),
     path('productsales/',views.getProductSales,name="productsales"),
+    path('generateProductReport/', views.generateProductReport, name='generateProductReport'),
+    path('generateMonthlyReport/', views.generateMonthlyReport, name='generateMonthlyReport')
 ]
--- reports/reportsapp/views.py
+++ reports/reportsapp/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpResponse,FileResponse
 from .models import monthlyanalysis,productanalysis
 from .serializers import MonthlySalesSerializer,ProductSalesSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.core import serializers
+from io import BytesIO
+from reportlab.pdfgen import canvas;
+
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -27,7 +30,34 @@ def getProductSales(request):
     serializer = ProductSalesSerializer(data_list, many=True)
     return Response(serializer.data)
 
+@api_view(['GET'])
+def generateProductReport(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename="productreport.pdf"'
+    buffer = BytesIO()
+    reportText = canvas.Canvas(buffer)
+    productReport = productanalysis.objects.all()
+    for product in productReport:
+        reportText.drawString(400,500,product.producttype)
+        reportText.showPage()
+    reportText.save()
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+    return response
 
-
-# Create your views here.
-
+@api_view(['GET'])
+def generateMonthlyReport(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename="customerreport.pdf"'
+    buffer = BytesIO()
+    reportText = canvas.Canvas(buffer)
+    monthReport = monthlyanalysis.objects.all()
+    for month in monthReport:
+        reportText.drawString(400,500,month.monthlist)
+        reportText.showPage()
+    reportText.save()
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+    return response
