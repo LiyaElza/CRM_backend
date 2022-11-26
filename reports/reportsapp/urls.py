ee9e4a002095240546303cbb384446d33ede84ef
Liya Elsa Mathew <liya.mathew@experionglobal.com>
2022-11-26 16:04:28 +0530

    Liya added models

--- reports/reportsapp/urls.py
+++ reports/reportsapp/urls.py
@@ -5,6 +5,8 @@ urlpatterns=[
     path('/',views.getRoutes,name="routes"),
     path('monthlysales/',views.getmonthlySales,name="monthlysales"),
     path('productsales/',views.getProductSales,name="productsales"),
+    path('customersales/',views.CustomerGrowth,name="customersales"),
     path('generateProductReport/', views.generateProductReport, name='generateProductReport'),
     path('generateMonthlyReport/', views.generateMonthlyReport, name='generateMonthlyReport')
+    
 ]
--- reports/reportsapp/views.py
+++ reports/reportsapp/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse,HttpResponse,FileResponse
 from .models import monthlyanalysis,productanalysis
+from crm.models import customers
+from crm.serializers import customersSerializer
 from .serializers import MonthlySalesSerializer,ProductSalesSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.core import serializers
 from io import BytesIO
-from reportlab.pdfgen import canvas;
+from reportlab.pdfgen import canvas
+from datetime import date
 
 
 @api_view(['GET'])
@@ -31,15 +34,36 @@ def getProductSales(request):
     return Response(serializer.data)
 
 @api_view(['GET'])
+def CustomerGrowth(request):
+    data_list = customers.objects.all()
+    export_list=[]
+    data_items={}
+    for item in data_list:
+        datem = item.joiningDate.strftime("%Y")
+        if datem in data_items:
+            data_items[datem]+=1
+        else:
+            data_items[datem]=1
+    for key,item in data_items.items():
+        item={"year":key,"number":item}
+        export_list.append(item)
+    return Response(export_list)
+
+
+
+@api_view(['GET'])
 def generateProductReport(request):
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = 'filename="productreport.pdf"'
     buffer = BytesIO()
     reportText = canvas.Canvas(buffer)
+    reportText.drawCentredString(reportText._pagesize[0] /2,reportText._pagesize[0]+200,"Product-Sales Analysis")
     productReport = productanalysis.objects.all()
+    x=400
+    y=500
     for product in productReport:
-        reportText.drawString(400,500,product.producttype)
-        reportText.showPage()
+        reportText.drawString(x+10,y+10,product.producttype)
+    reportText.showPage()    
     reportText.save()
     pdf = buffer.getvalue()
     buffer.close()
@@ -61,3 +85,5 @@ def generateMonthlyReport(request):
     buffer.close()
     response.write(pdf)
     return response
+
+
