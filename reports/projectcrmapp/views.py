18942ec686872a16ddeebfb2849fb1bc727df564
Liya Elsa Mathew <liya.mathew@experionglobal.com>
2022-11-30 02:49:47 +0530

    Liya Committed

--- reports/projectcrmapp/views.py
+++ reports/projectcrmapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework import status
 
 # Create your views here.
 from .serializers import HeroSerializer
@@ -23,8 +24,15 @@ def getroutes(request):
 
 
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def getproducts(request):
     data_list = productstable.objects.all()
-    serializer = HeroSerializer(data_list, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = HeroSerializer(data_list, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = HeroSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
