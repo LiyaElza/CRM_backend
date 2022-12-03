a872d05b75a00b822469286d48436ba9bf588a03
Aswanth M <aswanth.m@experionglobal.com>
2022-12-03 15:11:45 +0530

    Aswanth Committed

--- reports/projectcrmapp/views.py
+++ reports/projectcrmapp/views.py
@@ -3,6 +3,7 @@ from rest_framework import status
 
 # Create your views here.
 from .serializers import HeroSerializer
+from projectcrmapp.models import productstable
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
@@ -11,8 +12,9 @@ from rest_framework.response import Response
 # from .meals import meals
 from .models import productstable
 from django.core import serializers
-
-
+import pandas as pd
+from django.core.files.storage import FileSystemStorage
+import os
 
 @api_view(['GET'])
 def getroutes(request):
@@ -30,9 +32,27 @@ def getproducts(request):
     if request.method == 'GET':
         serializer = HeroSerializer(data_list, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = HeroSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # elif request.method == 'POST':
+    #     serializer = HeroSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def Import_csv(request):
+        if request.method == 'POST' and request.FILES['file']:
+            fs=FileSystemStorage()
+            filename=fs.save      
+            productexceldata = pd.read_excel(request.FILES['file'] )
+            # print(productexceldata)
+            dbframe = productexceldata
+            for dbframe in dbframe.itertuples():
+                 
+                obj = productstable.objects.create(id=dbframe.id,title=dbframe.title,
+                                                 category=dbframe.category, thiruvalla=dbframe.thiruvalla, kottayam=dbframe.kottayam,
+                                                kochi=dbframe.kochi, img=dbframe.img,
+                                               )
+               
+                obj.save()
+        return Response({'message':'File Added Successfully'})
